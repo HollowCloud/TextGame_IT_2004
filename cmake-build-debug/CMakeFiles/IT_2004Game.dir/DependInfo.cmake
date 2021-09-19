81b689c8bceed55c705c1204205e76c1c94801c6
HollowCloud <76275104+HollowCloud@users.noreply.github.com>
2021-09-19 15:23:21 +0600

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/IT_2004Game.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/asus/CLionProjects/IT-2004Game/Enemy.cpp" "C:/Users/asus/CLionProjects/IT-2004Game/cmake-build-debug/CMakeFiles/IT_2004Game.dir/Enemy.cpp.obj"
+  "C:/Users/asus/CLionProjects/IT-2004Game/Game.cpp" "C:/Users/asus/CLionProjects/IT-2004Game/cmake-build-debug/CMakeFiles/IT_2004Game.dir/Game.cpp.obj"
+  "C:/Users/asus/CLionProjects/IT-2004Game/MainCharacter.cpp" "C:/Users/asus/CLionProjects/IT-2004Game/cmake-build-debug/CMakeFiles/IT_2004Game.dir/MainCharacter.cpp.obj"
+  "C:/Users/asus/CLionProjects/IT-2004Game/main.cpp" "C:/Users/asus/CLionProjects/IT-2004Game/cmake-build-debug/CMakeFiles/IT_2004Game.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/IT_2004Game.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IT_2004Game.dir/Enemy.cpp.obj"
+  "CMakeFiles/IT_2004Game.dir/Game.cpp.obj"
+  "CMakeFiles/IT_2004Game.dir/MainCharacter.cpp.obj"
+  "CMakeFiles/IT_2004Game.dir/main.cpp.obj"
+  "IT_2004Game.exe"
+  "IT_2004Game.exe.manifest"
+  "IT_2004Game.lib"
+  "IT_2004Game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IT_2004Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
