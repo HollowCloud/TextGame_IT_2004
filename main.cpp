847c87b6e9b1cf0d1e13e5983fd030b8ff843e15
HollowCloud <76275104+HollowCloud@users.noreply.github.com>
2021-09-19 14:55:27 +0600

    Part_of_Tamerlan

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Game.h"
+#include "MainCharacter.h"
+
+using namespace std;
+
+int main() {
+    Game game;
+    game.MainMenu(); //HERE IS THE ONLY LINE, WE ARE APPROACHING TO MAINMENU
+
+}
